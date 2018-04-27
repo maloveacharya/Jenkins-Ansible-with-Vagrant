4f13602aaf0de85ae8681b8badd515af36829d49
maloveacharya <30867112+maloveacharya@users.noreply.github.com>
2018-04-27 08:36:46 -0400

    third comit

---
+++ add-java-to-mgmt.sh
@@ -0,0 +1,12 @@
+# install java8 (https://github.com/franzwong/til/blob/master/java/silent-install-oracle-jdk8-ubuntu.md)
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+# install Jenkins (https://wiki.jenkins.io/display/JENKINS/Installing+Jenkins+on+Ubuntu)
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
