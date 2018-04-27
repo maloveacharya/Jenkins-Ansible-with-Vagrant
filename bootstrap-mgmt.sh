94aa236ea9379d8e8e6412c115f6ae34ffe531d9
maloveacharya <30867112+maloveacharya@users.noreply.github.com>
2018-04-27 08:01:07 -0400

    second commit

---
+++ bootstrap-mgmt.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+
+# install java8 (https://github.com/franzwong/til/blob/master/java/silent-install-oracle-jdk8-ubuntu.md)
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+# install Jenkins (https://wiki.jenkins.io/display/JENKINS/Installing+Jenkins+on+Ubuntu)
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
+
+# copy examples into /home/vagrant (from inside the mgmt node)
+cp -a /vagrant/examples/* /home/vagrant
+chown -R vagrant:vagrant /home/vagrant
+
+# configure hosts file for our internal network defined by Vagrantfile
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.15.10  mgmt
+10.0.15.11  lb
+10.0.15.21  web1
+10.0.15.22  web2
+10.0.15.23  web3
+10.0.15.24  web4
+10.0.15.25  web5
+10.0.15.26  web6
+10.0.15.27  web7
+10.0.15.28  web8
+10.0.15.29  web9
+EOL
